a8fdc2d9984895dee23a99046f19b3fc8364f680
Antonina Vertsinskaya <tyndria23@gmail.com>
2017-12-04 22:22:59 +0300

    Update MergeLists.java

--- heaps-maps/MergeLists.java
+++ heaps-maps/MergeLists.java
@@ -15,33 +15,33 @@ public class MergeLists {
 		if (a.size() == 0) {
 			return null;
 		}
-        PriorityQueue<ListNode> queue = 
-            new PriorityQueue<ListNode>(a.size(), (n1, n2) -> n1.val - n2.val);
-        ListNode headNode = null;
-        ListNode currNode = null;
-        
-        for (int i = 0; i < a.size(); i ++) {
-           queue.offer(a.get(i));
-        }
-        
-        while (queue.size() > 0) {
-           ListNode top = queue.poll();
-           
-           if (headNode == null) {
-               headNode = top;
-               
-               currNode = top;
-           } else {
-               currNode.next = top;
-               
-               currNode = currNode.next;
-           }
-           
-           if (top.next != null) {
-               queue.offer(top.next);
-           }
-        }
-        
-        return headNode;
+		PriorityQueue<ListNode> queue = 
+		    new PriorityQueue<ListNode>(a.size(), (n1, n2) -> n1.val - n2.val);
+		ListNode headNode = null;
+		ListNode currNode = null;
+
+		for (int i = 0; i < a.size(); i ++) {
+		   queue.offer(a.get(i));
+		}
+
+		while (queue.size() > 0) {
+		   ListNode top = queue.poll();
+
+		   if (headNode == null) {
+		       headNode = top;
+
+		       currNode = top;
+		   } else {
+		       currNode.next = top;
+
+		       currNode = currNode.next;
+		   }
+
+		   if (top.next != null) {
+		       queue.offer(top.next);
+		   }
+		}
+
+		return headNode;
 	}
 }
