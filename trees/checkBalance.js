afda114f3ad40405e0c49cb8e8fa11d1414db244
tyndria <tyndria23@gmail.com>
2023-07-14 11:45:41 +0400

    solve binary search and tree problems

--- trees/checkBalance.js
+++ trees/checkBalance.js
@@ -1,4 +1,39 @@
 /* Given a binary tree, determine if it is height-balanced. */
+
+// version 2
+
+var isBalanced = function(root) {
+    if (!root) {
+        return true;
+    }
+
+    let isBalanced = true;
+
+    const recurse = (node) => {
+        if (!node) {
+            return 0;
+        }
+
+        const leftNodeHeight = recurse(node.left);
+        const rightNodeHeight = recurse(node.right);
+
+        if (!isBalanced) {
+            return;
+        }
+
+        if (Math.abs(leftNodeHeight - rightNodeHeight) <= 1) {
+            return Math.max(leftNodeHeight, rightNodeHeight) + 1;
+        } else {
+            isBalanced = false;
+        }
+    }
+
+    recurse(root);
+    
+    return isBalanced;
+};
+
+// version 1
 function isBalanced(A){
     var balanced = 1;
     var isRunning = true;
